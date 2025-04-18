b02354e880d4b7bb55ac8cd1a89175f804d3ef36
[daniamdrei] <[daniamdrei@gmail.com]>
2025-04-18 20:34:46 +0300

    login with gmail / facebook /apple

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\OrderItemController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\RestaurantController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\SocialAuthController;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Payment;
@@ -19,7 +20,7 @@ use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\DeliveryStatusesController;
 use App\Http\Controllers\API\DeliveryTrackingController;
 use App\Http\Controllers\API\FoodItemController;
-
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -37,6 +38,24 @@ Route::apiResource('payment', PaymentController::class);
 Route::apiResource('order', OrderController::class);
 Route::apiResource('orderItem', OrderItemController::class);
 Route::apiResource('notification', NotificationController::class);
+//auth routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('reset-password', [AuthController::class, 'resetPassword']);
 
+// Route with Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/user', function (Request $request) {
+    //     return $request->user();
+    // });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/user/profile', [UserController::class, 'updateProfile']);
+});
 
 //Route::post('addresses/{id}/restore', [AddressController::class, 'restore']);
+
+
+//login with gmail
+Route::post('/login/google/token', [SocialAuthController::class, 'loginWithGmailToken']);
+//login with facebook
+Route::post('/login/facebook/token', [SocialAuthController::class, 'loginWithFacebookToken']);
