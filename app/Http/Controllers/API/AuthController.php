a3941df8980896480cfbfbba9f9c7b4c1a52950b
[daniamdrei] <[daniamdrei@gmail.com]>
2025-04-25 14:40:49 +0300

    solve the conflict

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -11,73 +11,70 @@ use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\ResetPasswordRequest;
 use App\Http\Requests\UpdateProfileRequest;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
-// use Illuminate\Support\Facades\DB;
-// use Illuminate\Support\Facades\Password;
-use Illuminate\Support\Facades\Validator;
-// use Illuminate\Validation\ValidationException;
+// use Illuminate\Support\Carbon;
+// use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:100',
-            'email' => ['required','string','email','max:150','unique:users,email',
-                    'regex:/@((gmail|yahoo|hotmail|outlook)\.com)$/i',],
-            'birthday' =>'required|date|before_or_equal:' . Carbon::now()->subYears(16)->format('Y-m-d'),
-            'phone' => 'required|string|max:15',
-            'password' => ['required','string','min:6','confirmed',
-            'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/',
-    ],
-        ]);
-//'birthday' => 'required|date',
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-        // Creating the user
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'birthday' => $request->date_of_birth,
-            'phone' => $request->phone,
-            'password' => Hash::make($request->password),
-        ]);
+//     public function register(Request $request)
+//     {
+//         $validator = Validator::make($request->all(), [
+//             'name' => 'required|string|max:100',
+//             'email' => ['required','string','email','max:150','unique:users,email',
+//                     'regex:/@((gmail|yahoo|hotmail|outlook)\.com)$/i',],
+//             'birthday' =>'required|date|before_or_equal:' . Carbon::now()->subYears(16)->format('Y-m-d'),
+//             'phone' => 'required|string|max:15',
+//             'password' => ['required','string','min:6','confirmed',
+//             'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/',
+//     ],
+//         ]);
+// //'birthday' => 'required|date',
+//         if ($validator->fails()) {
+//             return response()->json(['errors' => $validator->errors()], 422);
+//         }
+//         // Creating the user
+//         $user = User::create([
+//             'name' => $request->name,
+//             'email' => $request->email,
+//             'birthday' => $request->date_of_birth,
+//             'phone' => $request->phone,
+//             'password' => Hash::make($request->password),
+//         ]);
 
-        return response()->json([
-            'message' => 'Your Account has been Created',
-            'User' => $user
-        ], 201);
-    }
+//         return response()->json([
+//             'message' => 'Your Account has been Created',
+//             'User' => $user
+//         ], 201);
+//     }
 
-    public function login(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]);
+//     public function login(Request $request)
+//     {
+//         $validator = Validator::make($request->all(), [
+//             'email' => 'required|string|email',
+//             'password' => 'required|string',
+//         ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-        // Attempt to log the user in
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json([
-                'message' => 'Invalid email or password'
-            ], 401);
-        }
+//         if ($validator->fails()) {
+//             return response()->json(['errors' => $validator->errors()], 422);
+//         }
+//         // Attempt to log the user in
+//         if (!Auth::attempt($request->only('email', 'password'))) {
+//             return response()->json([
+//                 'message' => 'Invalid email or password'
+//             ], 401);
+//         }
 
-        // Retrieve the user by email and generate the token
-        $user = User::where('email', $request->email)->firstOrFail();
-        $token = $user->createToken('auth_Token')->plainTextToken;
+//         // Retrieve the user by email and generate the token
+//         $user = User::where('email', $request->email)->firstOrFail();
+//         $token = $user->createToken('auth_Token')->plainTextToken;
 
-        return response()->json([
-            'message' => 'User Login Successfully',
-            'User' => $user,
-            'Token' => $token
-        ], 200);
-    }
+//         return response()->json([
+//             'message' => 'User Login Successfully',
+//             'User' => $user,
+//             'Token' => $token
+//         ], 200);
+//     }
 
     public function logout(Request $request)
     {
@@ -88,15 +85,4 @@ class AuthController extends Controller
         ]);
     }
 
-// public function updateProfile(UpdateProfileRequest $request)
-// {
-//     $user = auth()->user();
-
-//     $user->update($request->validated());
-
-//     return response()->json([
-//         'message' => 'Profile updated successfully.',
-//         'user' => $user
-//     ]);
-// }
 }
