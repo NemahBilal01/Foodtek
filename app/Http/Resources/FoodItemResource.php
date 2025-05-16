06c5467f5d79340781c3cf4e77fad644a50ec107
[daniamdrei] <[daniamdrei@gmail.com]>
2025-05-16 21:13:55 +0300

    FoddItemController

--- app/Http/Resources/FoodItemResource.php
+++ app/Http/Resources/FoodItemResource.php
@@ -16,24 +16,25 @@ class FoodItemResource extends JsonResource
     {
        return [
             'id' => $this->id,
-            'restaurant_id' => $this->restaurant_id,
-            'category_id' => $this->category_id,
-            'name' => $this->when(app()->getLocale() === 'ar' && $this->name_ar, $this->name_ar, $this->name),
-            'description' => $this->when(app()->getLocale() === 'ar' && $this->description_ar, $this->description_ar, $this->description),
+            // 'restaurant_id' => $this->restaurant_id,
+            // 'category_id' => $this->category_id,
+            // 'name' => $this->when(app()->getLocale() === 'ar' && $this->name_ar, $this->name_ar, $this->name),
+            // 'description' => $this->when(app()->getLocale() === 'ar' && $this->description_ar, $this->description_ar, $this->description),
+            'name' => app()->getLocale() === 'ar' ? $this->name_ar : $this->name_en,
+            'description' => app()->getLocale() === 'ar' ? $this->description_ar : $this->description_en,
             'price' => $this->price,
             'image_path' => $this->image_path,
             'is_available' => $this->is_available,
-
             'price_after_discount' => $this->whenLoaded('specialOffer', function () {
                 return $this->specialOffer
                     ? $this->price - ($this->price * $this->specialOffer->discount_percentage / 100)
                     : null;
             }),
-            
+
             'average_rating' => $this->when(isset($this->rating_avg), round($this->rating_avg, 1)),
             'number_of_reviews' => $this->when(isset($this->review_count), $this->review_count),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            // 'created_at' => $this->created_at,
+            // 'updated_at' => $this->updated_at,
         ];
     }
 }
